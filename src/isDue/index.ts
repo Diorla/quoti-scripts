f60e9de2a51186f51472f205201e1441708a00d3
Ade Adeola <adedotster@gmail.com>
2022-02-22 23:22:28 +0000

    documentation added

--- src/isDue/index.ts
+++ src/isDue/index.ts
@@ -3,52 +3,54 @@ import DateType from "../interfaces/DateType";
 import FrequencyType from "../interfaces/FrequencyType";
 import isRightInterval from "./isRightInterval";
 
+/**
+ * Confirm if the task is due based on the start date and current date
+ * @param  startDate the first date of the task
+ * @param currentDate Today's date
+ * @param frequencyType whether it is daily, weekly, monthly or yearly
+ * @param frequencyValue every x days, weeks, months or years
+ * @param dayOfWeek days of the week, 0 to 6, for weekly activity
+ * @returns {boolean} true or false
+ * @example isDue(new Date(), new Date(), "D", 1, []) // true
+ */
 export default function isDue(
   startDate: DateType,
-  compareDate: DateType,
+  currentDate: DateType,
   frequencyType: FrequencyType,
   frequencyValue: number,
   dayOfWeek: number[] = []
-) {
+): boolean {
   // daily check
   if (frequencyType === "D")
     return isRightInterval(
-      dayjs(compareDate).diff(startDate, "d"),
+      dayjs(currentDate).diff(startDate, "d"),
       frequencyValue
     );
 
   // weekly check
   if (frequencyType === "W") {
     const isTheWeek = isRightInterval(
-      dayjs(compareDate).diff(startDate, "w"),
+      dayjs(currentDate).diff(startDate, "w"),
       frequencyValue
     );
+
     return isTheWeek && dayOfWeek.includes(dayjs().day());
   }
 
   // monthly check
   if (frequencyType === "M") {
-    if ("development" === process.env.NODE_ENV) {
-      console.log(
-        startDate.toLocaleString(),
-        compareDate.toLocaleString(),
-        frequencyType,
-        frequencyValue
-      );
-    }
-
     const isThisMonth = isRightInterval(
-      dayjs(compareDate).diff(startDate, "m"),
+      dayjs(currentDate).diff(startDate, "m"),
       frequencyValue
     );
     const date = dayjs(startDate).date();
-    return isThisMonth && date === dayjs(compareDate).date();
+    return isThisMonth && date === dayjs(currentDate).date();
   }
 
   // yearly check
   if (frequencyType === "Y") {
     const isThisYear = isRightInterval(
-      dayjs(compareDate).diff(startDate, "y"),
+      dayjs(currentDate).diff(startDate, "y"),
       frequencyValue
     );
     const date = dayjs(startDate).date();
@@ -56,7 +58,7 @@ export default function isDue(
     const endMonth = dayjs(startDate).month();
     return (
       isThisYear &&
-      date === dayjs(compareDate).date() &&
+      date === dayjs(currentDate).date() &&
       startMonth === endMonth
     );
   }
--- src/isDue/isRightInterval.ts
+++ src/isDue/isRightInterval.ts
@@ -1,3 +1,9 @@
+/**
+ * Evaluate to see if the interval matches
+ * @param difference subtract start date from current date
+ * @param frequencyValue every x days, weeks, months or years
+ * @returns {boolean} true or false;
+ */
 export default function isRightInterval(
   difference: number,
   frequencyValue: number
