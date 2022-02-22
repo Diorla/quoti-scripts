ce9326678d3600bad96b6ddc17e0d2d910db9201
Ade Adeola <adedotster@gmail.com>
2022-02-22 11:15:06 +0000

    remove unused file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import isDue from "./isDue";
 
-export const sum = (a: number, b: number) => {
-  // if ("development" === process.env.NODE_ENV) {
-  //   console.log("beep");
-  // }
-  return a + b;
-};
-
 export { isDue };
